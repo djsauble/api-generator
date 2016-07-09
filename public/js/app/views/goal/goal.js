d524af1e7cdf6be3fb2391c922fc074893064c2b
Daniel Sauble <djsauble@gmail.com>
2016-07-08 20:36:49 -0700

    Create a form for setting goals (defunct)

--- public/js/app/views/goal/goal.js
+++ public/js/app/views/goal/goal.js
@@ -7,7 +7,25 @@ $(function(exports) {
         },
 
         render: function() {
-          this.$el.append("<div class='modal'><div>Set a goal! <a href='#'>All done.</a></div></div>");
+          var htmlString = "<div class='modal'>" +
+                           "<form>" +
+                           "<div class='field row'>" +
+                           "<label for='miles_per_week'>Miles per week</label>" +
+                           "<input id='miles_per_week' name='miles_per_mile' type='number'/>" +
+                           "</div>" +
+                           "<div class='field row'>" +
+                           "<label for='minutes_per_mile'>Minutes per mile</label>" +
+                           "<input id='minutes_per_mile' name='minutes_per_mile' type='number'/>" +
+                           "</div>" +
+                           "<buttons>" +
+                           "<button class='set_goal'>Set goal</button> " +
+                           "<a href='#'>Nevermind, go back</a>" +
+                           "</buttons>" +
+                           "</form>" +
+                           "</div>";
+
+          this.$el.append(htmlString);
+
           return this;
         }
       });
