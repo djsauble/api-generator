977dfeeeaa357e7bc0849252df2cae49ccd99ce3
Daniel Sauble <djsauble@gmail.com>
2016-07-21 16:15:56 -0700

    Persist data in cookies

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,23 +1,59 @@
-function updateToday(milesPerWeek) {
+var $ = require('jquery');
+var Cookie = require('tiny-cookie');
+
+$(function() {
+  // Configure events
+  $('#today').on('input', updateToday);
+  $('#goal').on('input', updateGoal);
+
+  // Load data from previous sessions
+  loadFromCookies();
+
+  // Update estimate if applicable
+  updateEstimate();
+});
+
+// Load persisted data from cookies, if available
+function loadFromCookies() {
+  var todayMilesPerWeek = Cookie.get('todayMilesPerWeek');
+  if (todayMilesPerWeek) {
+    document.querySelector('#today').value = todayMilesPerWeek;
+    updateToday();
+  }
+  var goalMilesPerWeek = Cookie.get('goalMilesPerWeek');
+  if (goalMilesPerWeek) {
+    document.querySelector('#goal').value = goalMilesPerWeek;
+    updateGoal();
+  }
+}
+
+// Update today's mileage
+function updateToday() {
+  var milesPerWeek = document.querySelector('#today').value;
   document.querySelector('#todayOutput').value = milesPerWeek;
+  updateEstimate();
+  Cookie.set('todayMilesPerWeek', milesPerWeek);
+}
+
+// Update the goal mileage
+function updateGoal() {
+  var milesPerWeek = document.querySelector('#goal').value;
+  document.querySelector('#goalOutput').value = milesPerWeek;
+  updateEstimate();
+  Cookie.set('goalMilesPerWeek', milesPerWeek);
+}
+
+// Update the time estimate
+function updateEstimate() {
   document.querySelector('#estimate').value = makeWeeksHuman(
     weeksToGoal(
-      parseFloat(milesPerWeek),
+      parseFloat(document.querySelector('#todayOutput').value),
       parseFloat(document.querySelector('#goalOutput').value)
     )
   );
 }
 
-function updateGoal(milesPerWeek) {
-  document.querySelector('#goalOutput').value = milesPerWeek;
-  document.querySelector('#estimate').value = makeWeeksHuman(
-    weeksToGoal(
-      parseFloat(document.querySelector('#todayOutput').value),
-      parseFloat(milesPerWeek)
-    )
-  );
-}
-
+// Convert weeks to a more appropriate timescale
 function makeWeeksHuman(weeks) {
   if (weeks < 14) {
     return Math.round(weeks) + ' weeks';
@@ -30,6 +66,7 @@ function makeWeeksHuman(weeks) {
   }
 }
 
+// Given a starting and ending mileage, calculate training time
 function weeksToGoal(startMileage, goalMileage) {
   var currentMileage = startMileage;
   var weekCount = 0;
@@ -53,6 +90,7 @@ function weeksToGoal(startMileage, goalMileage) {
   return weekCount;
 }
 
+// Never prescribe weekly mileage lower than 3
 function lessThan3(currentMileage) {
   return {
     milesPerWeek: 3,
@@ -60,6 +98,7 @@ function lessThan3(currentMileage) {
   }
 }
 
+// Increase weekly mileage by 1 until hitting 10 mpw
 function lessThan10(currentMileage) {
   return {
     milesPerWeek: currentMileage + 1,
@@ -67,6 +106,7 @@ function lessThan10(currentMileage) {
   }
 }
 
+// Increase weekly mileage by 10% per week until hitting 20 mpw
 function lessThan20(currentMileage) {
   return {
     milesPerWeek: currentMileage * 1.1,
@@ -74,6 +114,7 @@ function lessThan20(currentMileage) {
   }
 }
 
+// Increase weekly mileage at increasingly slow intervals after 20 mpw
 function moreThan20(currentMileage) {
   return {
     milesPerWeek: currentMileage * 1.1,
