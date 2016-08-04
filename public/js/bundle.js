d53772668f85fb1dbf02e7e2d2681cc27c25099a
Daniel Sauble <djsauble@gmail.com>
2016-08-04 15:52:48 -0700

    Ping the server every 30 seconds to keep it alive

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -205,14 +205,24 @@ var Backbone = require('backbone');
 var Socket = Backbone.Model.extend({
 
   initialize: function() {
-    var ws = new WebSocket(WEBSOCKET_URL);
+    var ws = new WebSocket(WEBSOCKET_URL),
+        keepAlive;
 
     // Tie websocket events to the event bus
     ws.onopen = function() {
+
+      // Register the client for server push notifications
       Forrest.bus.trigger('socket:send', 'client:register', {
         user: USER_ID,
         token: USER_TOKEN
       });
+
+      // Set up a ping every 30 seconds to keep the server alive (needed for Heroku)
+      keepAlive = setInterval(function() {
+        Forrest.bus.trigger('socket:send', 'client:ping', {});
+      }, 30000);
+
+      // Let the rest of the application know that the connection is ready for business
       Forrest.bus.trigger('socket:open', ws);
     };
     ws.onmessage = function(data, flags) {
@@ -221,6 +231,9 @@ var Socket = Backbone.Model.extend({
     };
     ws.onclose = function() {
       Forrest.bus.trigger('socket:close', ws);
+
+      // Kill the ping
+      clearInterval(keepAlive);
     };
     ws.onerror = function(error, more) {
       Forrest.bus.trigger('socket:error', ws, error);
